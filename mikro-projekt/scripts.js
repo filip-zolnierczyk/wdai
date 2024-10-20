5a67891ca17746527a0a68e49a5fd78c4ea35ab2
filip-zolnierczyk <filip.zolnierczyk@gmail.com>
2024-10-20 13:33:29 +0200

    almost done

--- mikro-projekt/scripts.js
+++ mikro-projekt/scripts.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const form = document.querySelector('.contact-form');
 
     form.addEventListener('submit', function(event) {
-        event.preventDefault(); // Zapobiega wysłaniu danych
-        alert("Formularz został poprawnie wysłany, ale dane nie będą przetwarzane.");
+        event.preventDefault(); 
+        alert("Formularz został poprawnie wysłany.");
     });
 });
